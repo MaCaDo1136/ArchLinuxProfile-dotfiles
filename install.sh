5f5f2911422f336665a2599acbc1cff37b1b4d09
Mario Casas <a01287116@tec.mx>
2025-06-07 18:45:56 -0600

    Actualizo configuraciones: Hyprland, kitty, zsh, waybar y más

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+echo "Instalando symlinks..."
+
+ln -sf ~/.dotfiles/zsh/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/zsh/p10k.zsh ~/.p10k.zsh
 ln -sf ~/.dotfiles/bashrc ~/.bashrc
 ln -sf ~/.dotfiles/hypr ~/.config/hypr
+ln -sf ~/.dotfiles/waybar ~/.config/waybar
+
+echo "Hecho."
