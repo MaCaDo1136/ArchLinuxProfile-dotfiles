2b3a225428eccf5f853b9ed0be6ec678f90728cd
Mario Casas <A01287116@tec.mx>
2025-07-09 13:06:50 -0600

    Install.sh debug

--- install.sh
+++ install.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-set -e  # Salir si hay error
+echo "Actualizando base y paquetes..."
+sudo pacman -Syu
 
-echo "==== Dotfiles Installer ===="
+echo "Instalando paquetes de pacman desde pkglist.txt..."
+sudo pacman -S --needed $(cat ~/.dotfiles/setup/pkglist.txt)
 
-echo "[1/4] Instalando paquetes..."
-bash setup/install_packages.sh
+echo "Verificando si yay está instalado..."
+if ! command -v yay &> /dev/null; then
+    echo "yay no está instalado. Instalando yay..."
+    git clone https://aur.archlinux.org/yay.git ~/build/yay
+    cd ~/build/yay && makepkg -si --noconfirm
+fi
 
-echo "[2/4] Creando symlinks..."
-bash setup/symlinks.sh
+echo "Instalando paquetes AUR desde aurlist.txt..."
+yay -S --needed $(cat ~/.dotfiles/setup/aurlist.txt)
 
-echo "[3/4] Instalando SDDM..."
-bash setup/install_sddm_thme.sh
-
-echo "[4/4] Ejecutando configuraciones post-instalación..."
-bash setup/post_install.sh
-
-echo "✅ Instalación completada."
