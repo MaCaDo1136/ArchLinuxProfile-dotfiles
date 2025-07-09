f84b53e08f22462498ba7fb8a62c1b9c1ff35a74
Mario Casas <A01287116@tec.mx>
2025-07-09 12:29:09 -0600

    Cambios de /setup

--- setup/install_packages.sh
+++ setup/install_packages.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-echo "Instalando paquetes de pacman..."
+echo "Actualizando base y paquetes..."
 sudo pacman -Syu
-sudo pacman -Syu --needed - < pkglist.txt
 
-echo "Instalando paquetes AUR..."
+echo "Instalando paquetes de pacman desde pkglist.txt..."
+sudo pacman -S --needed $(cat ~/.dotfiles/setup/pkglist.txt)
+
+echo "Verificando si yay está instalado..."
 if ! command -v yay &> /dev/null; then
     echo "yay no está instalado. Instalando yay..."
     git clone https://aur.archlinux.org/yay.git ~/build/yay
     cd ~/build/yay && makepkg -si --noconfirm
 fi
 
-yay -S --needed - < aurlist.txt
+echo "Instalando paquetes AUR desde aurlist.txt..."
+yay -S --needed $(cat ~/.dotfiles/setup/aurlist.txt)
